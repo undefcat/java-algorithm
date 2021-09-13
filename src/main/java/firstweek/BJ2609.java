e5e6028feaff5372ff32421d7d40fccf94bb4fe8
undefcat <undefcat@gmail.com>
2021-09-13 09:34:54 +0900

    2609, 10430

---
+++ src/main/java/firstweek/BJ2609.java
@@ -0,0 +1,47 @@
+package firstweek;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ2609 {
+    public static void main(String[] args) throws Throwable {
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in)
+        );
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a < b) {
+            int tmp = a;
+
+            a = b;
+            b = tmp;
+        }
+
+        int gcd = getGcd(a, b);
+        int lcm = getLcm(a, b, gcd);
+
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
+
+    private static int getGcd(int a, int b) {
+        while (true) {
+            int mod = a%b;
+            if (mod == 0) {
+                return b;
+            }
+
+            a = b;
+            b = mod;
+        }
+    }
+
+    private static int getLcm(int a, int b, int gcd) {
+        return (a*b) / gcd;
+    }
+}
