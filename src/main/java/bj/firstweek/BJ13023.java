9f24905f5daef81095393aae9e3e14817ca07e4c
undefcat <undefcat@gmail.com>
2021-09-16 00:41:52 +0900

    fix 13023

--- src/main/java/bj/firstweek/BJ13023.java
+++ src/main/java/bj/firstweek/BJ13023.java
@@ -33,12 +33,12 @@ public class BJ13023 {
             nodes[b].neighbors.add(nodes[a]);
         }
 
+        // 시작점으로부터 5개의 노드를 방문하지 못한 경우
+        // 그 다음 시작점부터 다시 시작하는데,
+        // 이 때 visited를 초기화하지 않으면
+        // 그 다음 노드의 탐색이 제대로 이루어지지 않는다!
         for (Node n: nodes) {
-            if (n.visited) {
-                continue;
-            }
-
-            n.dfs(0);
+            n.dfs(1);
         }
 
         System.out.println("0");
@@ -48,21 +48,25 @@ public class BJ13023 {
         boolean visited = false;
         List<Node> neighbors = new LinkedList<>();
 
-        public void dfs(int depth) {
-            depth += 1;
-            if (depth >= 5) {
+        public void dfs(int visit) {
+            // 5개의 노드를 만났다면
+            // A -> B -> C -> D -> E 의 관계가 성립하는 것이다!
+            if (visit >= 5) {
                 System.out.println("1");
                 System.exit(0);
             }
+
+            visited = true;
 
             for (Node n: neighbors) {
                 if (n.visited) {
                     continue;
                 }
 
-                n.visited = true;
-                n.dfs(depth);
+                n.dfs(visit+1);
             }
+
+            visited = false;
         }
     }
 }
