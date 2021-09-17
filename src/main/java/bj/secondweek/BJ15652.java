5b1f3e1d8d8a12bfc7f7693551ef49c651e685e3
undefcat <undefcat@gmail.com>
2021-09-18 00:11:55 +0900

    solve(15652): N과 M (4)

---
+++ src/main/java/bj/secondweek/BJ15652.java
@@ -0,0 +1,52 @@
+package bj.secondweek;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ15652 {
+    private static final BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+    );
+
+    private static final StringBuilder sb = new StringBuilder(100_000);
+
+    private static int N;
+    private static int M;
+
+    private static int[] picked;
+
+    public static void main(String[] args) throws Throwable {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        picked = new int[M];
+
+        solve(0, 1);
+
+        System.out.print(sb);
+    }
+
+    private static void solve(int index, int startNum) {
+        if (index == M) {
+            appendPicked();
+            return;
+        }
+
+        for (int n = startNum; n <= N; n++) {
+            picked[index] = n;
+            solve(index+1, n);
+        }
+    }
+
+    private static void appendPicked() {
+        for (int i = 0; i < M; i++) {
+            sb.append(picked[i]);
+            sb.append(' ');
+        }
+
+        sb.append('\n');
+    }
+}
