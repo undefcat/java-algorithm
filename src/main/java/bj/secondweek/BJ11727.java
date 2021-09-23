908bbfa5337c34895020ef10565cf399d4d73eb9
undefcat <undefcat@gmail.com>
2021-09-24 08:51:14 +0900

    refactor(11727): 1_001 -> 상수로 사용하도록 변경

--- src/main/java/bj/secondweek/BJ11727.java
+++ src/main/java/bj/secondweek/BJ11727.java
@@ -12,13 +12,13 @@ public class BJ11727 {
     private static final int MOD = 10_007;
 
     public static void main(String[] args) throws Throwable {
-        int[] dp = new int[1_001];
+        int[] dp = new int[MAX_VALUE];
 
         dp[0] = 1;
         dp[1] = 1;
         dp[2] = 3;
 
-        for (int n = 3; n < 1_001; n++) {
+        for (int n = 3; n < MAX_VALUE; n++) {
             dp[n] = dp[n-1] + (2*dp[n-2]);
             dp[n] %= MOD;
         }
