3da949a3aa145c6e64636a24a69f9f12b6806867
undefcat <undefcat@gmail.com>
2021-10-02 00:46:20 +0900

    solve(2164): 카드2

---
+++ src/main/java/bj/class2/BJ2164.java
@@ -0,0 +1,29 @@
+package bj.class2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class BJ2164 {
+    private static final BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+    );
+
+    public static void main(String[] args) throws Throwable {
+        final int N = Integer.parseInt(br.readLine());
+
+        final Deque<Integer> deque = new ArrayDeque<>(N);
+
+        for (int n = 1; n <= N; n++) {
+            deque.add(n);
+        }
+
+        while (deque.size() > 1) {
+            deque.remove();
+            deque.addLast(deque.remove());
+        }
+
+        System.out.println(deque.remove());
+    }
+}
