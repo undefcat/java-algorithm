f64a983b528f3412664602c80033a0af34dfeac9
undefcat <undefcat@gmail.com>
2021-10-02 21:32:15 +0900

    solve(10773): 제로

---
+++ src/main/java/bj/class2/BJ10773.java
@@ -0,0 +1,33 @@
+package bj.class2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class BJ10773 {
+    private static final BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+    );
+
+    public static void main(String[] args) throws Throwable {
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<Integer> queue = new ArrayDeque<>(N);
+
+        long sum = 0;
+        for (int ni = 0; ni < N; ni++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                sum -= queue.pop();
+                continue;
+            }
+
+            queue.push(num);
+            sum += num;
+        }
+
+        System.out.println(sum);
+    }
+}
