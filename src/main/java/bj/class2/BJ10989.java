798d0fd342657433a8916d1e6e31bfa04d955897
undefcat <undefcat@gmail.com>
2021-10-02 21:47:14 +0900

    solve(10989): 수 정렬하기 3

---
+++ src/main/java/bj/class2/BJ10989.java
@@ -0,0 +1,35 @@
+package bj.class2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class BJ10989 {
+    private static final BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in), 1<<20
+    );
+
+    private static final BufferedWriter bw = new BufferedWriter(
+            new OutputStreamWriter(System.out), 1<<20
+    );
+
+    public static void main(String[] args) throws Throwable {
+        final int N = Integer.parseInt(br.readLine());
+
+        final int[] numbers = new int[N];
+        for (int n = 0; n < N; n++) {
+            numbers[n] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(numbers);
+
+        for (int num: numbers) {
+            bw.write(Integer.toString(num));
+            bw.write('\n');
+        }
+
+        bw.flush();
+    }
+}
