173812ef0f35f930826fa7be6886b82450d38123
undefcat <undefcat@gmail.com>
2021-10-16 23:05:49 +0900

    solve(14002): 가장 긴 증가하는 부분 수열 4

---
+++ src/main/java/bj/comito/codeplus/basic/week06/BJ14002.java
@@ -0,0 +1,73 @@
+package bj.comito.codeplus.basic.week06;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ14002 {
+    private static int N;
+    private static int[] sequence;
+
+    public static void main(String[] args) throws Throwable {
+        input();
+
+        System.out.println(dp());
+    }
+
+    private static void input() throws Throwable {
+        final BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in), 1<<15
+        );
+
+        N = Integer.parseInt(br.readLine());
+        sequence = new int[N];
+
+        final StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int ni = 0; ni < N; ni++) {
+            sequence[ni] = Integer.parseInt(st.nextToken());
+        }
+
+        br.close();
+    }
+
+    private static String dp() {
+        final int[] nextIndex = new int[N];
+        final int[] length = new int[N];
+
+        Arrays.fill(nextIndex, -1);
+        Arrays.fill(length, 1);
+
+        int maxLength = 0;
+        int maxIndex = -1;
+
+        for (int ni = N-1; ni >= 0; ni--) {
+            for (int nj = ni; nj < N; nj++) {
+                if (sequence[nj] > sequence[ni]) {
+                    final int candi = length[nj] + 1;
+
+                    if (length[ni] < candi) {
+                        length[ni] = candi;
+                        nextIndex[ni] = nj;
+                    }
+                }
+            }
+
+            if (length[ni] > maxLength) {
+                maxLength = length[ni];
+                maxIndex = ni;
+            }
+        }
+
+        final StringBuilder sb = new StringBuilder(1000*1000);
+        int startIndex = maxIndex;
+        while (startIndex >= 0) {
+            sb.append(sequence[startIndex]);
+            sb.append(' ');
+            startIndex = nextIndex[startIndex];
+        }
+
+        return sb.toString();
+    }
+}
